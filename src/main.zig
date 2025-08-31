8dca1233578779866347a2eb7cd1ec695e881f63
HeavenTao <542248377@qq.com>
2025-08-31 21:50:10 +0800

    1.sync

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
-const TermSize = @import("termSize.zig");
+const getTermSize = @import("termSize.zig").getTermSize;
 const print = std.debug.print;
 
-pub fn main() !void {}
+pub fn main() !void {
+    const size = getTermSize();
+    if (size == null) {
+        @panic("no term size");
+    }
+}
