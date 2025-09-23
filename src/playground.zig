c1cd9153d6617d6bcc90fc3148a234f82d15a9a7
HeavenTao <542248377@qq.com>
2025-09-23 17:16:59 +0800

    1.add egg check ,wall check

--- src/playground.zig
+++ src/playground.zig
@@ -11,6 +11,23 @@ const Rect = struct { x: u16, y: u16, w: u16, h: u16 };
 
 const Point = struct { x: u16, y: u16 };
 
+const Egg = struct {
+    position: Point,
+
+    fn init() Egg {
+        return .{ .position = .{ .x = 0, .y = 0 } };
+    }
+
+    fn resetPosition(self: *Egg, w: u16, h: u16) void {
+        const r: u64 = @intCast(std.time.nanoTimestamp());
+        var prng = std.Random.DefaultPrng.init(r);
+        var random = prng.random();
+        const eggX = random.uintLessThan(u16, w);
+        const eggY = random.uintLessThan(u16, h);
+        self.position = .{ .x = eggX, .y = eggY };
+    }
+};
+
 const Snake = struct {
     allocator: Allocator,
     body: std.ArrayList(Point),
@@ -23,6 +40,12 @@ const Snake = struct {
         try body.append(allocator, .{ .x = 8, .y = 3 });
 
         return .{ .allocator = allocator, .body = body };
+    }
+
+    pub fn eat(self: *Snake, egg: *Egg) !void {
+        const eggPosition = egg.position;
+
+        try self.body.append(self.allocator, .{ .x = eggPosition.x, .y = eggPosition.y });
     }
 
     fn bodyMove(self: *Snake) void {
@@ -69,9 +92,10 @@ pub const Playground = struct {
     size: Size = .{ .h = 0, .w = 0 },
     wall: Rect = .{ .x = 1, .y = 1, .w = 0, .h = 0 },
     snake: Snake,
+    egg: Egg,
 
     pub fn init(allocator: Allocator) !Playground {
-        return .{ .allocator = allocator, .snake = try Snake.init(allocator) };
+        return .{ .allocator = allocator, .snake = try Snake.init(allocator), .egg = Egg.init() };
     }
 
     pub fn start(self: *Playground) !void {
@@ -113,7 +137,10 @@ pub const Playground = struct {
                 }
             }
 
-            {}
+            //draw egg
+            {
+                _ = try stdout.write(try drawer.drawPoint(allocator, self.egg.position.x, self.egg.position.y, null));
+            }
 
             _ = try stdout.write(cursor.home);
 
@@ -142,8 +169,41 @@ pub const Playground = struct {
             } else if (byte == 's') {
                 self.snake.moveDown();
             }
+
+            //check wall
+            if (!self.checkWall()) {
+                break;
+            }
+
+            if (self.checkEgg()) {
+                self.egg.resetPosition(self.size.w, self.size.h);
+                try self.snake.eat(&self.egg);
+            }
         } else |err| {
             std.debug.print("err,{}", .{err});
+        }
+    }
+
+    fn checkWall(self: *Playground) bool {
+        const snake_head = self.snake.body.items[0];
+        if (snake_head.x == self.wall.x) {
+            return false;
+        } else if (snake_head.y == self.wall.y) {
+            return false;
+        } else if (snake_head.x == (self.wall.x + self.wall.w - 1)) {
+            return false;
+        } else if (snake_head.y == (self.wall.y + self.wall.h - 1)) {
+            return false;
+        }
+        return true;
+    }
+
+    fn checkEgg(self: *Playground) bool {
+        const snake_head = self.snake.body.items[0];
+        if (snake_head.x == self.egg.position.x and snake_head.y == self.egg.position.y) {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -161,6 +221,8 @@ pub const Playground = struct {
             .w = self.size.w,
             .h = self.size.h,
         };
+
+        self.egg.resetPosition(self.size.w, self.size.h);
     }
 
     fn hideCursor() !void {
